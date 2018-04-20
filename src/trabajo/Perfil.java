926f6d04acc2aaba08f85a8b1b5ec174d8df0de3
anton <anton@DESKTOP-9CI6CMG>
2018-04-20 17:39:41 +0200

    Nueva entidad

---
+++ src/trabajo/Perfil.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author anton
+ */
+@Entity
+public class Perfil implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long rol;
+
+    public Long getId() {
+        return rol;
+    }
+
+    public void setId(Long id) {
+        this.rol = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (rol != null ? rol.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Perfil)) {
+            return false;
+        }
+        Perfil other = (Perfil) object;
+        if ((this.rol == null && other.rol != null) || (this.rol != null && !this.rol.equals(other.rol))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "trabajo.Perfil[ id=" + rol + " ]";
+    }
+    
+}
