f9a67d702a837e04ddd90ab58b2af9472c20fc7b
josealonso <jose.9642@hotmail.com>
2018-04-22 18:06:15 +0200

    he añadido fecha_del_pago a pago_cuota
    he añadido fecha_del_pago a pago_cuota

--- src/trabajo/Pago_cuota.java
+++ src/trabajo/Pago_cuota.java
@@ -6,11 +6,15 @@
 package trabajo;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
@@ -25,7 +29,12 @@ public class Pago_cuota implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(nullable=false)
     private Integer cantidad_pagada;
+    @Temporal(TemporalType.DATE)
+    @Column(nullable=false)
+    private Date fecha_del_pago;
+    @Column(nullable=false)
     private String Tipo_pago;
 
     public void setTipo_pago(String Tipo_pago) {
